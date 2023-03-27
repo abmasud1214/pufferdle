af2d79030080b442e61deba895d27cdc44130eee
abmasud1214 <abmasud1214@gmail.com>
2023-03-26 23:56:11 -0700

    Added all fish data.

--- src/fishdata.js
+++ src/fishdata.js
@@ -6,7 +6,494 @@ export default {
             "behavior": "Floater",
             "season": ["Summer"],
             "weather": ["Sun"],
-            "time": [12, 16]
+            "time": [12, 16],
+            "location": ["Ocean"]
         },
+        {
+            "name": "Anchovy",
+            "difficulty": 30,
+            "behavior": "Dart",
+            "season": ["Spring", "Fall"],
+            "weather": ["Any"],
+            "time": [6, 26],
+            "location": ["Ocean"]
+        },
+        {
+            "name": "Tuna",
+            "difficulty": 70,
+            "behavior": "Smooth",
+            "season": ["Summer", "Winter"],
+            "weather": ["Any"],
+            "time": [6, 19],
+            "location": ["Ocean"]
+        },
+        {
+            "name": "Sardine",
+            "difficulty": 30,
+            "behavior": "Dart",
+            "season": ["Spring", "Fall", "Winter"],
+            "weather": ["Any"],
+            "time": [6, 19],
+            "location": ["Ocean"]
+        },
+        {
+            "name": "Bream",
+            "difficulty": 35,
+            "behavior": "Smooth",
+            "season": ["Spring", "Summer", "Fall", "Winter"],
+            "weather": ["Any"],
+            "time": [18, 26],
+            "location": ["Town", "Forest"]
+        },
+        {
+            "name": "Largemouth Bass",
+            "difficulty": 50,
+            "behavior": "Mixed",
+            "season": ["Spring", "Summer", "Fall", "Winter"],
+            "weather": ["Any"],
+            "time": [6, 19],
+            "location": ["Mountain Lake"]
+        },
+        {
+            "name": "Smallmouth Bass",
+            "difficulty": 28,
+            "behavior": "Mixed",
+            "season": ["Spring", "Fall"],
+            "weather": ["Any"],
+            "time": [6, 26],
+            "location": ["Town", "Pond"]
+        },
+        {
+            "name": "Rainbow Trout",
+            "difficulty": 45,
+            "behavior": "Mixed",
+            "season": ["Summer"],
+            "weather": ["Sun"],
+            "time": [6, 19],
+            "location": ["Town", "Forest", "Lake"]
+        },
+        {
+            "name": "Salmon",
+            "difficulty": 50,
+            "behavior": "Mixed",
+            "season": ["Fall"],
+            "weather": ["Any"],
+            "time": [6, 19],
+            "location": ["Town", "Forest"]
+        },
+        {
+            "name": "Walleye",
+            "difficulty": 45,
+            "behavior": "Smooth",
+            "season": ["Fall", "Winter"],
+            "weather": ["Rain"],
+            "time": [12, 26],
+            "location": ["Town", "Forest", "Pond", "Lake"]
+        },
+        {
+            "name": "Perch",
+            "difficulty": 35,
+            "behavior": "Dart",
+            "season": ["Winter"],
+            "weather": ["Any"],
+            "time": [6, 26],
+            "location": ["Town", "Forest", "Pond", "Lake"]
+        },
+        {
+            "name": "Carp",
+            "difficulty": 15,
+            "behavior": "Mixed",
+            "season": ["Spring", "Summer", "Fall", "Winter"],
+            "weather": ["Any"],
+            "time": [6, 26],
+            "location": ["Sewers", "Woods", "Lake"]
+        },
+        {
+            "name": "Catfish",
+            "difficulty": 75,
+            "behavior": "Mixed",
+            "season": ["Spring", "Fall"],
+            "weather": ["Rain"],
+            "time": [6, 12],
+            "location": ["Town", "Forest", "Woods", "Swamp"]
+        },
+        {
+            "name": "Pike",
+            "difficulty": 60,
+            "behavior": "Dart",
+            "season": ["Summer", "Winter"],
+            "weather": ["Any"],
+            "time": [6, 26],
+            "location": ["Town", "Forest", "Pond"]
+        },
+        {
+            "name": "Sunfish",
+            "difficulty": 30,
+            "behavior": "Mixed",
+            "season": ["Spring", "Summer"],
+            "weather": ["Sun", "Wind"],
+            "time": [6, 19],
+            "location": ["Town", "Forest"]
+        },
+        {
+            "name": "Red Mullet",
+            "difficulty": 55,
+            "behavior": "Smooth",
+            "season": ["Summer", "Winter"],
+            "weather": ["Any"],
+            "time": [6, 19],
+            "location": ["Ocean"]
+        },
+        {
+            "name": "Herring",
+            "difficulty": 25,
+            "behavior": "Dart",
+            "season": ["Spring", "Winter"],
+            "weather": ["Any"],
+            "time": [6, 26],
+            "location": ["Ocean"]
+        },
+        {
+            "name": "Eel",
+            "difficulty": 70,
+            "behavior": "Smooth",
+            "season": ["Spring", "Fall"],
+            "weather": ["Rain"],
+            "time": [16, 26],
+            "location": ["Ocean"]
+        },
+        {
+            "name": "Octopus",
+            "difficulty": 95,
+            "behavior": "Sinker",
+            "season": ["Summer"],
+            "weather": ["Any"],
+            "time": [6, 13],
+            "location": ["Ocean", "Market"]
+        },
+        {
+            "name": "Red Snapper",
+            "difficulty": 40,
+            "behavior": "Mixed",
+            "season": ["Summer", "Fall"],
+            "weather": ["Rain"],
+            "time": [6, 19],
+            "location": ["Ocean"]
+        },
+        {
+            "name": "Squid",
+            "difficulty": 75,
+            "behavior": "Sinker",
+            "season": ["Winter"],
+            "weather": ["Any"],
+            "time": [18, 26],
+            "location": ["Ocean"]
+        },
+        {
+            "name": "Sea Cucumber",
+            "difficulty": 40,
+            "behavior": "Sinker",
+            "season": ["Fall", "Winter"],
+            "weather": ["Any"],
+            "time": [6, 19],
+            "location": ["Ocean", "Market"]
+        },
+        {
+            "name": "Super Cucumber",
+            "difficulty": 80,
+            "behavior": "Sinker",
+            "season": ["Summer", "Fall"],
+            "weather": ["Any"],
+            "time": [18, 26],
+            "location": ["Ocean", "Ginger Ocean", "Market"]
+        },
+        {
+            "name": "Ghostfish",
+            "difficulty": 50,
+            "behavior": "Mixed",
+            "season": ["Spring","Summer","Fall", "Winter"],
+            "weather": ["Any"],
+            "time": [6, 26],
+            "location": ["Mines"]
+        },
+        {
+            "name": "Stonefish",
+            "difficulty": 65,
+            "behavior": "Sinker",
+            "season": ["Spring","Summer","Fall", "Winter"],
+            "weather": ["Any"],
+            "time": [6, 26],
+            "location": ["Mines"]
+        },
+        {
+            "name": "Ice Pip",
+            "difficulty": 85,
+            "behavior": "Dart",
+            "season": ["Spring","Summer","Fall", "Winter"],
+            "weather": ["Any"],
+            "time": [6, 26],
+            "location": ["Mines"]
+        },
+        {
+            "name": "Lava Eel",
+            "difficulty": 90,
+            "behavior": "Mixed",
+            "season": ["Spring","Summer","Fall", "Winter"],
+            "weather": ["Any"],
+            "time": [6, 26],
+            "location": ["Mines", "Volcano"]
+        },
+        {
+            "name": "Sandfish",
+            "difficulty": 65,
+            "behavior": "Mixed",
+            "season": ["Spring","Summer","Fall", "Winter"],
+            "weather": ["Any"],
+            "time": [6, 20],
+            "location": ["Desert"]
+        },
+        {
+            "name": "Scorpion Carp",
+            "difficulty": 90,
+            "behavior": "Dart",
+            "season": ["Spring","Summer","Fall", "Winter"],
+            "weather": ["Any"],
+            "time": [6, 20],
+            "location": ["Desert"]
+        },
+        {
+            "name": "Flounder",
+            "difficulty": 50,
+            "behavior": "Sinker",
+            "season": ["Spring","Summer"],
+            "weather": ["Any"],
+            "time": [6, 20],
+            "location": ["Ocean", "Ginger Ocean"]
+        },
+        {
+            "name": "Midnight Carp",
+            "difficulty": 55,
+            "behavior": "Mixed",
+            "season": ["Fall","Winter"],
+            "weather": ["Any"],
+            "time": [20, 26],
+            "location": ["Lake", "Pond", "Ginger Pond", "Ginger River"]
+        },
+        {
+            "name": "Sturgeon",
+            "difficulty": 78,
+            "behavior": "Mixed",
+            "season": ["Summer","Winter"],
+            "weather": ["Any"],
+            "time": [6, 19],
+            "location": ["Lake"]
+        },
+        {
+            "name": "Tiger Trout",
+            "difficulty": 60,
+            "behavior": "Dart",
+            "season": ["Fall","Winter"],
+            "weather": ["Any"],
+            "time": [6, 19],
+            "location": ["Town", "Forest"]
+        },
+        {
+            "name": "Bullhead",
+            "difficulty": 46,
+            "behavior": "Smooth",
+            "season": ["Spring","Summer","Fall","Winter"],
+            "weather": ["Any"],
+            "time": [6, 26],
+            "location": ["Lake"]
+        },
+        {
+            "name": "Tilapia",
+            "difficulty": 50,
+            "behavior": "Mixed",
+            "season": ["Summer","Fall"],
+            "weather": ["Any"],
+            "time": [6, 14],
+            "location": ["Ocean", "Ginger River"]
+        },
+        {
+            "name": "Chub",
+            "difficulty": 35,
+            "behavior": "Dart",
+            "season": ["Spring","Summer","Fall","Winter"],
+            "weather": ["Any"],
+            "time": [6, 26],
+            "location": ["Forest", "Lake"]
+        },
+        {
+            "name": "Dorado",
+            "difficulty": 78,
+            "behavior": "Mixed",
+            "season": ["Summer"],
+            "weather": ["Any"],
+            "time": [6, 19],
+            "location": ["Forest"]
+        },
+        {
+            "name": "Albacore",
+            "difficulty": 60,
+            "behavior": "Mixed",
+            "season": ["Fall","Winter"],
+            "weather": ["Any"],
+            "time": [6, 11],
+            "location": ["Ocean"]
+        },
+        {
+            "name": "Shad",
+            "difficulty": 45,
+            "behavior": "Smooth",
+            "season": ["Spring", "Summer", "Fall"],
+            "weather": ["Rain"],
+            "time": [9, 26],
+            "location": ["Town", "Forest"]
+        },
+        {
+            "name": "Lingcod",
+            "difficulty": 85,
+            "behavior": "Mixed",
+            "season": ["Winter"],
+            "weather": ["Any"],
+            "time": [6, 26],
+            "location": ["Town", "Forest", "Lake"]
+        },
+        {
+            "name": "Halibut",
+            "difficulty": 50,
+            "behavior": "Sinker",
+            "season": ["Spring", "Summer","Winter"],
+            "weather": ["Any"],
+            "time": [6, 11],
+            "location": ["Ocean"]
+        },
+        {
+            "name": "Woodskip",
+            "difficulty": 50,
+            "behavior": "Mixed",
+            "season": ["Spring", "Summer","Fall","Winter"],
+            "weather": ["Any"],
+            "time": [6, 26],
+            "location": ["Woods"]
+        },
+        {
+            "name": "Void Salmon",
+            "difficulty": 80,
+            "behavior": "Mixed",
+            "season": ["Spring", "Summer","Fall","Winter"],
+            "weather": ["Any"],
+            "time": [6, 26],
+            "location": ["Swamp"]
+        },
+        {
+            "name": "Slimejack",
+            "difficulty": 55,
+            "behavior": "Dart",
+            "season": ["Spring","Summer","Fall","Winter"],
+            "weather": ["Any"],
+            "time": [6, 26],
+            "location": ["Bug"]
+        },
+        {
+            "name": "Stingray",
+            "difficulty": 80,
+            "behavior": "Sinker",
+            "season": ["Spring","Summer","Fall","Winter"],
+            "weather": ["Any"],
+            "time": [6, 26],
+            "location": ["Cove"]
+        },
+        {
+            "name": "Lionfish",
+            "difficulty": 50,
+            "behavior": "Smooth",
+            "season": ["Spring","Summer","Fall","Winter"],
+            "weather": ["Any"],
+            "time": [6, 26],
+            "location": ["Ginger Ocean"]
+        },
+        {
+            "name": "Blue Discus",
+            "difficulty": 60,
+            "behavior": "Dart",
+            "season": ["Spring","Summer","Fall","Winter"],
+            "weather": ["Any"],
+            "time": [6, 26],
+            "location": ["Ginger Pond", "Ginger River"]
+        },
+        {
+            "name": "Midnight Squid",
+            "difficulty": 55,
+            "behavior": "Sinker",
+            "season": ["Winter"],
+            "weather": ["Any"],
+            "time": [17, 26],
+            "location": ["Market"]
+        },
+        {
+            "name": "Spook Fish",
+            "difficulty": 60,
+            "behavior": "Dart",
+            "season": ["Winter"],
+            "weather": ["Any"],
+            "time": [17, 26],
+            "location": ["Market"]
+        },
+        {
+            "name": "Blobfish",
+            "difficulty": 75,
+            "behavior": "Floater",
+            "season": ["Winter"],
+            "weather": ["Any"],
+            "time": [17, 26],
+            "location": ["Market"]
+        },
+        {
+            "name": "Crimsonfish",
+            "difficulty": 95,
+            "behavior": "Mixed",
+            "season": ["Summer"],
+            "weather": ["Any"],
+            "time": [6, 26],
+            "location": ["Ocean"]
+        },
+        {
+            "name": "Angler",
+            "difficulty": 85,
+            "behavior": "Smooth",
+            "season": ["Fall"],
+            "weather": ["Any"],
+            "time": [6, 26],
+            "location": ["Town"]
+        },
+        {
+            "name": "Legend",
+            "difficulty": 110,
+            "behavior": "Mixed",
+            "season": ["Spring"],
+            "weather": ["Rain"],
+            "time": [6, 26],
+            "location": ["Lake"]
+        },
+        {
+            "name": "Glacierfish",
+            "difficulty": 100,
+            "behavior": "Mixed",
+            "season": ["Winter"],
+            "weather": ["Any"],
+            "time": [6, 26],
+            "location": ["Forest"]
+        },
+        {
+            "name": "Mutant Carp",
+            "difficulty": 80,
+            "behavior": "Dart",
+            "season": ["Spring","Summer","Fall","Winter"],
+            "weather": ["Any"],
+            "time": [6, 26],
+            "location": ["Sewers"]
+        }
     ]
 }

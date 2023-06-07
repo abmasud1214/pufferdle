789bc1b6c30d4bb227eedb67615290ed450de235
abmasud1214 <abmasud1214@gmail.com>
2023-06-06 19:30:26 -0700

    React Router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,60 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
 
+import {
+    createBrowserRouter,
+    RouterProvider,
+    useNavigate,
+} from "react-router-dom";
+
 import './App.css';
 import Pufferdle from "./Pufferdle";
 import FishTank from "./Components/FishTank";
+import ErrorPage from "./ErrorPage";
 
 const pufferfish_src = require("./Art/pufferfish.png")
 const mutant_src = require("./Art/mutant_carp.png")
 const fishtank_src = require("./Art/fishtank.png")
 
+function RootElem() {
+    const navigate = useNavigate();
+    return (
+        <div className="menuButtons">
+            <div className="menuButton"> 
+                <img src={pufferfish_src} alt="/"/> 
+                <h1>Daily</h1>
+            </div>
+            <div className="menuButton" onClick={() => navigate("/random")}> 
+                <img src={mutant_src} alt=""/> 
+                <h1>Random</h1>
+            </div>
+            <div className="menuButton" onClick={() => navigate("/fishtank")}> 
+                <img src={fishtank_src} alt=""/> 
+                <h1>Fish Tank</h1>
+            </div>                
+        </div>
+    )
+}
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <RootElem />,
+        errorElement: <ErrorPage />,
+    },
+    {
+        path: "/daily",
+        element: <Pufferdle daily={true} />,
+    },
+    {
+        path: "/random",
+        element: <Pufferdle daily={false}/>,
+    },
+    {
+        path: "/fishtank",
+        element: <FishTank />,
+    }
+]);
 
 function App() {
 
@@ -16,23 +62,7 @@ function App() {
 
     return (
         <div className="App">
-            {inGame == 0 && <div className="menuButtons">
-                <div className="menuButton"> 
-                    <img src={pufferfish_src} alt=""/> 
-                    <h1>Daily</h1>
-                </div>
-                <div className="menuButton" onClick={() => setInGame(2)}> 
-                    <img src={mutant_src} alt=""/> 
-                    <h1>Random</h1>
-                </div>
-                <div className="menuButton" onClick={() => setInGame(3)}> 
-                    <img src={fishtank_src} alt=""/> 
-                    <h1>Fish Tank</h1>
-                </div>                
-            </div>}
-            {inGame == 1 && <Pufferdle daily={true}/>}
-            {inGame == 2 && <Pufferdle daily={false}/>}
-            {inGame == 3 && <FishTank />}
+            <RouterProvider router={router}/>
         </div>
     );
 }

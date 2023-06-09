2d1e56c2b6ea2e79b4fb66266ceea9bf14a95550
abmasud1214 <abmasud1214@gmail.com>
2023-06-08 21:46:18 -0700

    Fixed React Router | NavBar | Home Button

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import {
     createBrowserRouter,
     RouterProvider,
     useNavigate,
+    Outlet,
 } from "react-router-dom";
 
 import './App.css';
@@ -16,12 +17,44 @@ const pufferfish_src = require("./Art/pufferfish.png")
 const mutant_src = require("./Art/mutant_carp.png")
 const fishtank_src = require("./Art/fishtank.png")
 
-function RootElem() {
+const warptotem_src = require("./Art/farmtotem.png")
+const journal_src = require("./Art/journal.png")
+const notes_src = require("./Art/notes.png")
+const cog_src = require("./Art/cog.png")
+
+function RootLayout() {
+    const navigate = useNavigate();
+
+    return (
+        <div className="Page">
+            <header>
+                <div onClick={() => {navigate("/")}}>
+                    <img src={warptotem_src} alt="Home"/>
+                </div>
+                {Array.from(Array(8)).map(() => (<div></div>))}
+                <div>
+                    <img src={journal_src} alt="How to Play"/>
+                </div>
+                <div>
+                    <img src={notes_src} alt="Statistics"/>
+                </div>
+                <div>
+                    <img src={cog_src} alt="Settings"/>
+                </div>
+            </header>
+            <div className="App">
+                <Outlet />
+            </div>
+        </div>
+    )
+}
+
+function HomeElem() {
     const navigate = useNavigate();
     return (
         <div className="menuButtons">
             <div className="menuButton"> 
-                <img src={pufferfish_src} alt="/"/> 
+                <img src={pufferfish_src} alt=""/> 
                 <h1>Daily</h1>
             </div>
             <div className="menuButton" onClick={() => navigate("/random")}> 
@@ -39,31 +72,32 @@ function RootElem() {
 const router = createBrowserRouter([
     {
         path: "/",
-        element: <RootElem />,
+        element: <RootLayout />,
+        children: [
+            {
+                path: "/",
+                element: <HomeElem />
+            },
+            {
+                path: "/daily",
+                element: <Pufferdle daily={true} />,
+            },
+            {
+                path: "/random",
+                element: <Pufferdle daily={false}/>,
+            },
+            {
+                path: "/fishtank",
+                element: <FishTank />,
+            },
+        ],
         errorElement: <ErrorPage />,
     },
-    {
-        path: "/daily",
-        element: <Pufferdle daily={true} />,
-    },
-    {
-        path: "/random",
-        element: <Pufferdle daily={false}/>,
-    },
-    {
-        path: "/fishtank",
-        element: <FishTank />,
-    }
 ]);
 
 function App() {
-
-    const [inGame, setInGame] = React.useState(0);
-
     return (
-        <div className="App">
-            <RouterProvider router={router}/>
-        </div>
+        <RouterProvider router={router} />
     );
 }
 

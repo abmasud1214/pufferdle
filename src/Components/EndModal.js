cb4fff8b618bbad6f3bbd5dc3a43acea270ec1cc
abmasud1214 <abmasud1214@gmail.com>
2023-08-05 16:19:40 -0700

    vowels

--- src/Components/EndModal.js
+++ src/Components/EndModal.js
@@ -34,7 +34,7 @@ function StatScreen() {
 
     // console.log(stats);
     return (
-        <div style={{margin: "10px 0px"}}>
+        <div style={{margin: "10px 0px", display: "flex", flexDirection: "column", gap: "3px"}}>
             <StatBar ng={1} num={stats[1]} max={max}/>
             <StatBar ng={2} num={stats[2]} max={max}/>
             <StatBar ng={3} num={stats[3]} max={max}/>
@@ -53,34 +53,36 @@ export default function EndModal(props) {
     
     const [clipboardText, setClipboardText] = React.useState("");
 
-    // React.useEffect(() => {
-    //     const copyContent = async () => {
-    //         try {
-    //             await navigator.clipboard.writeText(clipboardText);
-    //             console.log("Content copied to clipboard");
-    //         } catch (err) {
-    //             console.error("Failed to copy: ", err);
-    //         }
-    //     }
-    //     copyContent();
-    // }, [clipboardText])
+    React.useEffect(() => {
+        if (clipboardText !== ""){
+            const copyContent = async () => {
+                try {
+                    await navigator.clipboard.writeText(clipboardText);
+                    console.log("Content copied to clipboard");
+                } catch (err) {
+                    console.error("Failed to copy: ", err);
+                }
+            }
+            copyContent();
+        }
+    }, [clipboardText])
 
     return (
         <div className="background" onClick={onClose}>
             <div className="modal" onClick={e => e.stopPropagation()}>
                 <div>
                     {correct && <h1>Congratulations!</h1>}
-                    {!correct && <h1>Unfortunate</h1>}
+                    {!correct && <h1>Nice Try</h1>}
                     <h2>{fishResults.caught ? 
-                        `You caught a${targetFish.name.toLowerCase()[0] === "a" ? "n" : ""}` 
-                        : `The fish was a${targetFish.name.toLowerCase()[0] === "a" ? "n" : ""}`} {targetFish.name}</h2>
-                    <div className="resultImages">
-                        {fishResults.caught && <img src={fish_img.src} alt="🎣"/>}
-                        {fishResults.treasure && <img src={treasure_img.src} alt="👑"/>}
-                        {fishResults.perfect && <img src={perfect_img.src} alt="⭐"/>}
-                    </div>
+                        `You caught a${/[aeiou]/.test(targetFish.name.toLowerCase()[0]) ? "n" : ""}` 
+                        : `The fish was a${/[aeiou]/.test(targetFish.name.toLowerCase()[0]) ? "n" : ""}`} {targetFish.name}</h2>
                 </div>
-                <StatScreen />
+                <div className="resultImages">
+                    {fishResults.caught && <img src={fish_img.src} alt="🎣"/>}
+                    {fishResults.treasure && <img src={treasure_img.src} alt="👑"/>}
+                    {fishResults.perfect && <img src={perfect_img.src} alt="⭐"/>}
+                </div>
+                {daily && <StatScreen />}
                 <div>
                     <button onClick={() => {
                         setClipboardText(guessesToString(guesses, numGuess, fishResults, daily, 0, false));

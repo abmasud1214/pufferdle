cb3c7e51ce7d6965b5e895f5f875d98e209e43aa
abmasud1214 <abmasud1214@gmail.com>
2023-08-15 18:10:16 -0700

    trying to fix router error.

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from "react"
 
 import {
     createHashRouter,
-    createBrowserRouter,
     RouterProvider,
     useNavigate,
     Outlet,
--- src/ErrorPage.js
+++ src/ErrorPage.js
@@ -7,7 +7,7 @@ export default function ErrorPage() {
   return (
     <div id="error-page">
       <h1>Oops!</h1>
-      <p>Sorry, an unexpected error has occurred.</p>
+      <p>Sorry, an unexpected error has occurred. (React Router)</p>
       <p>
         <i>{error.statusText || error.message}</i>
       </p>
